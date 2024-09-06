2e4a73bc68040257346e8c282eecb53e9097a2ba
Dave Gilson <gilsond@gmail.com>
2024-09-06 14:32:17 +1000

    Initial

---
+++ README.md
@@ -0,0 +1,36 @@
+# Crime Busters Upgrade
+This is an upgrade of simple code I developed to show maps of  crimes reported to police.
+
+Worked with QPS to organise a download of reports and created  `.xml` files.
+
+## Notes
+I used multiple resources to find and download the HTML and related.
+
+Ended up directly downloadeding the code, removing Wayback Machine code and links:
+
+https://web.archive.org/web/*/http://static.townsvillebulletin.com.au/crimebusters/*`
+
+Specific Date:
+
+https://web.archive.org/web/20111213050643if_/http://static.townsvillebulletin.com.au/crimebusters/#
+
+### I tried other ways:
+
+#### Bash Script
+
+```bash
+#!/bin/bash
+url= "http://www.townsvillebulletin.com.au/crimebusters/" 
+wget "http://web.archive.org/cdx/search/cdx?url=${url}*&output=json&fl=original,timestamp" -O out.json
+sed -Eni '2,$s%^\["([^"]*)","([^"]*)"](,|])$%https://web.archive.org/web/\2id_/\1%gmp' out.json 
+wget -i out.json
+```
+
+### Wayback Machine Downloader
+Source: https://github.com/hartator/wayback-machine-downloader
+
+wayback_machine_downloader http://www.townsvillebulletin.com.au/crimebusters/ --directory dev/cb --from 20110822050130 
+
+
+\\wsl.localhost\fedora\home\gilsond\development\my-recovered-websites\web.archive.org\web\20111213050643js_\http\static.townsvillebulletin.com.au
+
---
+++ scripts/querystring.js
@@ -0,0 +1,76 @@
+﻿var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/**
+    Created by: David Gilson 27/01/2009
+    
+    Processes the query strings and allows access by named key
+    
+    Contructor:
+        QueryStringProcessor()
+              Processes the current pages url storing the unescaped query strings as key/value pair
+    
+    Method(s):   
+        String get(String key) 
+              Returns the query string value for the specified key.           
+*/
+function QueryStringProcessor ()
+{
+    var querystrings = [];
+    
+    var url = window.location.search;
+
+	if (url != "") 
+	{
+        var data = url.substr(1);
+	    var splitted = data.split("&");
+    	
+	    for (i=0;i<=splitted.length-1;i++)
+	    {
+		    var items = splitted[i].split("=");
+		    querystrings[items[0].toLowerCase()] = unescape(items[1]);
+	    }
+	}
+    
+    this.get = function (key)
+                {
+                    var s = querystrings[key.toLowerCase()];
+                    
+                    if (typeof(s) == "undefined")
+                        s = "";
+                                
+                    return s;
+                }
+}   
+    
+
+}
+/*
+     FILE ARCHIVED ON 20:02:21 Mar 17, 2012 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 01:51:23 Sep 06, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.94
+  exclusion.robots: 0.049
+  exclusion.robots.policy: 0.037
+  esindex: 0.039
+  cdx.remote: 23.121
+  LoadShardBlock: 281.876 (3)
+  PetaboxLoader3.datanode: 241.827 (4)
+  PetaboxLoader3.resolve: 91.784 (2)
+  load_resource: 82.373
+*/
